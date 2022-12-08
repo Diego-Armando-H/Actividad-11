ce2c45142b90afb53da33ae9a6ec04d3c18d44b6
Diego-Armando-H <you@example.com>
2022-12-07 22:07:06 -0600

    Subida de la actividad 11

---
+++ src/listaParticulas.py
@@ -0,0 +1,118 @@
+import json
+from Particula import Particula
+from algoritmos import distancia_euclidiana
+
+
+class listaParticula:
+    def __init__(self):
+        self.__particulas = []
+
+    def agregar_inicio(self, particula: Particula):
+        self.__particulas.insert(0, particula)
+
+    def agregar_final(self, particula: Particula):
+        self.__particulas.append(particula)
+
+    def mostrar(self):
+        for particula in self.__particulas:
+            print(particula)
+
+    def __str__(self):
+        return "".join(
+            str(particula) for particula in self.__particulas
+        )
+
+    def guardar(self, ubicacion):
+        try:
+            with open(ubicacion, 'w') as archivo:
+                lista = [particula.to_dict()
+                         for particula in self.__particulas]
+
+                json.dump(lista, archivo, indent=5)
+            return 1
+        except:
+            return 0
+
+    def abrir(self, ubicacion):
+        try:
+            with open(ubicacion, 'r') as archivo:
+                lista = json.load(archivo)
+                self.__particulas = [Particula(**particula)
+                                     for particula in lista]
+            return 1
+        except:
+            return 0
+
+    def __len__(self):
+        return len(self.__particulas)
+
+    def __iter__(self):
+        self.cont = 0
+        return self
+
+    def __next__(self):
+        if self.cont < len(self.__particulas):
+            """ Asignamos la particula a devolver """
+            particula = self.__particulas[self.cont]
+            """ Incremenamos el contador """
+            self.cont += 1
+            return particula
+        """ detemos la iteración si se sobrepasa el tamaño de la lista """
+        raise StopIteration
+
+    def sortById(self):
+        self.__particulas.sort()
+
+    def sortByDistance(self):
+        self.__particulas.sort(key=sort_distance, reverse=True)
+
+    def sortBySpeed(self):
+        self.__particulas.sort(key=sort_speed)
+
+    """ Metodo de busqueda por fuerza bruta """
+
+    def puntos_cercanos(self):
+        """ Almacenará los resultados de la detección """
+        resultado = []
+        for punto_i in self.__particulas:
+            x = punto_i.origen_x
+            y = punto_i.origen_y
+            """ utilizamos una distancia considerablemente grande para forzar que el punto
+             de comparación siempre tenga una distancia inferior al inicio, despues se obtendrá
+             el punto verdaderamente más cercano"""
+            minimo = 1000
+            cercano = (0, 0)
+            color = (punto_i.red,
+                     punto_i.green,
+                     punto_i.blue)
+            """ Segundo recorrido para hacer la comparación con el elemento actual """
+            for punto_j in self.__particulas:
+                """ Evitamos comparar el punto inicial con el mismo """
+                if (punto_i != punto_j):
+                    """ Guardamos los nuevos puntos de comparación """
+                    x_comparison = punto_j.origen_x
+                    y_comparison = punto_j.origen_y
+                    """ Obtenemos la distancia euclidiana entre los 2 puntos para determinar si es
+                        mayor o menor """
+                    d = distancia_euclidiana(x, y, x_comparison, y_comparison)
+                    if d < minimo:
+                        """ Corregimos el valor de la distancia minima para proximas comparaciones """
+                        minimo = d
+                        """ Asignamos al punto más cercanos con los valores de comparación actuales """
+                        cercano = (x_comparison, y_comparison)
+            """ Agregamos al arreglo el punto inicial de comparación y depues el punto que se
+            detectó como el más cercano """
+            resultado.append(((x, y), cercano, color))
+        """ Finalmente retornamos los resultados obtenidos """
+        return resultado
+
+
+""" Metodos fuera de "lista particula" """
+
+
+def sort_distance(particula):
+    return particula.distancia
+
+
+def sort_speed(particula):
+    return particula.velocidad
---
+++ src/mainwindow.py
@@ -0,0 +1,253 @@
+from ui_mainwindow import Ui_MainWindow, QFileDialog, QMessageBox, QTableWidgetItem, QPen, QColor, QGraphicsScene
+from PySide2.QtWidgets import QMainWindow
+from PySide2.QtCore import Slot
+from listaParticulas import listaParticula
+from Particula import Particula
+
+
+class MainWindow(QMainWindow):
+    __contador = 0
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.__lista = listaParticula()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btnAgregarInicio.clicked.connect(self.click_agregar_inicio)
+        self.ui.btnAgregarFinal.clicked.connect(self.click_agregar_final)
+        self.ui.btnMostrar.clicked.connect(self.mostrar)
+        """ Metodos para el menu de la ventana """
+        self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
+        self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
+        """ Metodos para ordenar """
+        self.ui.actionSortId.triggered.connect(self.sortById)
+        self.ui.actionSortDistance.triggered.connect(self.sortByDistance)
+        self.ui.actionSortSpeed.triggered.connect(self.sortBySpeed)
+        """ Metodos para encontrar la cercanía en los puntos de origen """
+        self.ui.actionOnlyOrigenXY.triggered.connect(self.action_solo_origenes)
+        self.ui.actionNearestPoints.triggered.connect(
+            self.dibujar_mas_cercanos)
+        """ Metodos para trabajar con la tabla """
+        self.ui.mostrar_pushButton.clicked.connect(self.mostrar_tabla)
+        self.ui.buscar_pushButton.clicked.connect(self.buscar_id_tabla)
+
+        self.ui.dibujarPushButton.clicked.connect(self.dibujar)
+        self.ui.limpiarPushBtn.clicked.connect(self.limpiar)
+
+        self.scene = QGraphicsScene()
+        self.ui.particulasView.setScene(self.scene)
+
+    @Slot()
+    def dibujar(self):
+        """ Dibujamos todas las particulas a la vez """
+        for particula in self.__lista:
+            pen = QPen()
+            pen.setWidth(2)
+
+            color = QColor(particula.red, particula.green, particula.blue)
+            pen.setColor(color)
+
+            self.scene.addEllipse(float(particula.origen_x),
+                                  float(particula.origen_y), 3, 3, pen)
+            self.scene.addEllipse(float(particula.destino_x),
+                                  float(particula.destino_y), 3, 3, pen)
+            self.scene.addLine(float(particula.origen_x), float(particula.origen_y),
+                               float(particula.destino_x), float(particula.destino_y), pen)
+
+    def wheelEvent(self, event):
+        if event.delta() > 0:
+            self.ui.particulasView.scale(1.2, 1.2)
+            return
+        self.ui.particulasView.scale(0.8, 0.8)
+
+    @Slot()
+    def limpiar(self):
+        self.scene.clear()
+
+    @Slot()
+    def mostrar_tabla(self):
+        self.ui.tableParticulas.setColumnCount(10)
+        headers = ["id", "origen_x", "origen_y", "destino_x",
+                   "destino_y", "velocidad", "red", "green", "blue", "distancia"]
+        self.ui.tableParticulas.setHorizontalHeaderLabels(headers)
+        self.ui.tableParticulas.setRowCount(len(self.__lista))
+        """ Empezamos a rellenar la tabla """
+        row = 0
+        for particula in self.__lista:
+            id_widget = QTableWidgetItem(str(particula.id))
+            origen_x_widget = QTableWidgetItem(str(particula.origen_x))
+            origen_y_widget = QTableWidgetItem(str(particula.origen_y))
+            destino_x_widget = QTableWidgetItem(str(particula.destino_x))
+            destino_y_widget = QTableWidgetItem(str(particula.destino_y))
+            velocidad_widget = QTableWidgetItem(str(particula.velocidad))
+            red_widget = QTableWidgetItem(str(particula.red))
+            green_widget = QTableWidgetItem(str(particula.green))
+            blue_widget = QTableWidgetItem(str(particula.blue))
+            distancia_widget = QTableWidgetItem(str(particula.distancia))
+
+            self.ui.tableParticulas.setItem(row, 0, id_widget)
+            self.ui.tableParticulas.setItem(row, 1, origen_x_widget)
+            self.ui.tableParticulas.setItem(row, 2, origen_y_widget)
+            self.ui.tableParticulas.setItem(row, 3, destino_x_widget)
+            self.ui.tableParticulas.setItem(row, 4, destino_y_widget)
+            self.ui.tableParticulas.setItem(row, 5, velocidad_widget)
+            self.ui.tableParticulas.setItem(row, 6, red_widget)
+            self.ui.tableParticulas.setItem(row, 7, green_widget)
+            self.ui.tableParticulas.setItem(row, 8, blue_widget)
+            self.ui.tableParticulas.setItem(row, 9, distancia_widget)
+
+            row += 1
+
+    @Slot()
+    def buscar_id_tabla(self):
+        idBusqueda = self.ui.searchEdit.text()
+
+        for particula in self.__lista:
+            if idBusqueda == str(particula.id):
+                self.ui.tableParticulas.clear()
+                self.ui.tableParticulas.setColumnCount(10)
+                headers = ["id", "origen_x", "origen_y", "destino_x",
+                           "destino_y", "velocidad", "red", "green", "blue", "distancia"]
+                self.ui.tableParticulas.setHorizontalHeaderLabels(headers)
+                self.ui.tableParticulas.setRowCount(1)
+
+                id_widget = QTableWidgetItem(str(particula.id))
+                origen_x_widget = QTableWidgetItem(str(particula.origen_x))
+                origen_y_widget = QTableWidgetItem(str(particula.origen_y))
+                destino_x_widget = QTableWidgetItem(str(particula.destino_x))
+                destino_y_widget = QTableWidgetItem(str(particula.destino_y))
+                velocidad_widget = QTableWidgetItem(str(particula.velocidad))
+                red_widget = QTableWidgetItem(str(particula.red))
+                green_widget = QTableWidgetItem(str(particula.green))
+                blue_widget = QTableWidgetItem(str(particula.blue))
+                distancia_widget = QTableWidgetItem(str(particula.distancia))
+
+                self.ui.tableParticulas.setItem(0, 0, id_widget)
+                self.ui.tableParticulas.setItem(0, 1, origen_x_widget)
+                self.ui.tableParticulas.setItem(0, 2, origen_y_widget)
+                self.ui.tableParticulas.setItem(0, 3, destino_x_widget)
+                self.ui.tableParticulas.setItem(0, 4, destino_y_widget)
+                self.ui.tableParticulas.setItem(0, 5, velocidad_widget)
+                self.ui.tableParticulas.setItem(0, 6, red_widget)
+                self.ui.tableParticulas.setItem(0, 7, green_widget)
+                self.ui.tableParticulas.setItem(0, 8, blue_widget)
+                self.ui.tableParticulas.setItem(0, 9, distancia_widget)
+
+                return
+        QMessageBox.warning(
+            self,
+            "Error",
+            f'No se ha encontrado una particula con el id: "{idBusqueda}"'
+        )
+
+    """ Generación de eventos para acciones del menu """
+    @Slot()
+    def action_guardar_archivo(self):
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            "Guardar archivo",
+            ".",
+            "JSON (*.json)"
+        )[0]
+        if self.__lista.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Exito",
+                ("Se pudo crear el archivo " + ubicacion)
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                ("No pudo crear el archivo " + ubicacion)
+            )
+
+    @Slot()
+    def action_abrir_archivo(self):
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            "Guardar archivo",
+            ".",
+            "JSON (*.json)"
+        )[0]
+        if self.__lista.abrir(ubicacion):
+            QMessageBox.information(
+                self,
+                "Exito",
+                ("Se pudo abrir el archivo " + ubicacion)
+            )
+            self.ui.plainTextEdit.clear()
+            self.ui.plainTextEdit.insertPlainText(str(self.__lista))
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                ("No pudo abrir el archivo " + ubicacion)
+            )
+
+    """ Generación de metodos para la cercanía d4e los puntos """
+    @Slot()
+    def action_solo_origenes(self):
+        self.limpiar()
+        """ Dibujamos todas las particulas a la vez """
+        for particula in self.__lista:
+            pen = QPen()
+            pen.setWidth(2)
+
+            color = QColor(particula.red, particula.green, particula.blue)
+            pen.setColor(color)
+
+            self.scene.addEllipse(float(particula.origen_x),
+                                  float(particula.origen_y), 3, 3, pen)
+
+    @Slot()
+    def dibujar_mas_cercanos(self):
+        puntos_cercanos = self.__lista.puntos_cercanos()
+        for punto1, punto2, color in puntos_cercanos:
+            pen = QPen()
+            pen.setWidth(2)
+
+            c = QColor(color[0], color[1], color[2])
+            pen.setColor(c)
+            self.scene.addLine(float(punto1[0]), float(punto1[1]),
+                               float(punto2[0]), float(punto2[1]), pen)
+
+    @ Slot()
+    def click_agregar_inicio(self):
+        self.__lista.agregar_inicio(self.procesarParticula())
+        self.__contador += 1
+
+    @ Slot()
+    def click_agregar_final(self):
+        self.__lista.agregar_final(self.procesarParticula())
+        self.__contador += 1
+
+    @ Slot()
+    def mostrar(self):
+        self.ui.plainTextEdit.clear()
+        self.ui.plainTextEdit.insertPlainText(str(self.__lista))
+
+    @ Slot()
+    def sortById(self):
+        self.__lista.sortById()
+
+    @ Slot()
+    def sortByDistance(self):
+        self.__lista.sortByDistance()
+
+    @ Slot()
+    def sortBySpeed(self):
+        self.__lista.sortBySpeed()
+
+    def procesarParticula(self):
+        """ id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue, distancia """
+        return Particula(self.__contador,
+                         self.ui.spnnOrigenX.value(),
+                         self.ui.spnnOrigenY.value(),
+                         self.ui.spnnDestinoX.value(),
+                         self.ui.spnnDestinoY.value(),
+                         int(self.ui.spnnVelocidad.text()),
+                         self.ui.spnnRed.value(),
+                         self.ui.spnnBlue.value(),
+                         self.ui.spnnGreen.value(),
+                         self.ui.spnnDistancia.value())
